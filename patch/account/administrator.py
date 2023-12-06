d013a0f3b1b17132c5ac2b93603ff32d7355e441
EUN <617julie@korea.ac.kr>
2023-12-06 17:22:23 +0900

    add W-01 code

---
+++ patch/account/administrator.py
@@ -0,0 +1,21 @@
+# W-01 : 1. 계정관리 > 1.1 Administrator 계정 이름 변경 또는 보안성 강화
+
+import subprocess
+
+def checkadname():
+    result = subprocess.run(['powershell', 'Get-LocalUser Administrator | Select-Object -ExpandProperty Name'], capture_output=True, text=True)
+    return result.stdout.strip()
+
+def chadminame(newname):
+    
+    if checkadname() == "Administrator":
+        subprocess.run(['powershell', f'Rename-LocalUser -Name "Administrator" -NewName "{newname}"'])
+        print("Administrator name changed")
+
+
+# 결과 확인 코드
+current_name = checkadname()
+print(f"Administrator name: {current_name}")
+chadminame("NewAdminName")
+changed_name = checkadname()
+print(f"changed Administrator name: {changed_name}")
