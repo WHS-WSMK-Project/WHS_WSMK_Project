90ee967fcfa97f466d7e8a9ef74f02a1a61c0389
EUN <617julie@korea.ac.kr>
2023-12-09 05:19:45 +0900

    Update administrator.py

--- patch/account/administrator.py
+++ patch/account/administrator.py
@@ -1,6 +1,7 @@
 # W-01 : 1. 계정관리 > 1.1 Administrator 계정 이름 변경 또는 보안성 강화
 
 import subprocess
+import sys
 
 def checkadname():
     result = subprocess.run(['powershell', 'Get-LocalUser Administrator | Select-Object -ExpandProperty Name'], capture_output=True, text=True)
@@ -10,7 +11,10 @@ def chadminame(newname):
     
     if checkadname() == "Administrator":
         subprocess.run(['powershell', f'Rename-LocalUser -Name "Administrator" -NewName "{newname}"'])
-        print("Administrator name changed")    
+        print("Administrator name changed")  
+    else:
+        print("No User Named Administrator") 
+        sys.exit()
 
 
 # 결과 확인 코드
