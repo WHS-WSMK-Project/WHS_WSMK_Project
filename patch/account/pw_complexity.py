e25debe874437ddfce0d6d458c391c5e75f4fcf7
EUN <617julie@korea.ac.kr>
2023-12-27 15:10:07 +0900

    pw_complexity 완성

--- patch/account/pw_complexity.py
+++ patch/account/pw_complexity.py
@@ -6,43 +6,36 @@ import re
 def change_pwcomplexity():
     # 현재 시스템의 보안 설정을 내보내기
     subprocess.run('secedit /export /cfg C:\\winsec.inf', check=True, shell=True)
-    try:
         # 보안 설정 파일을 열어 pattern 과 일치하는 항목 찾기
-        with open("C:\\winsec.inf", 'r', encoding="utf-16") as file:
-            data = file.read()
-        pattern = r'PasswordComplexity\s*=\s*(\d)'
-        match = re.search(pattern, data)
+    with open("C:\\winsec.inf", 'r', encoding="utf-16") as file:
+        data = file.read()
+    pattern = r'PasswordComplexity\s*=\s*(\d)'
+    match = re.search(pattern, data)
 
-        # 보안 설정 파일을 열어 pattern 과 일치하는 항목 찾기
-        if match and match.group(1) == '1':
-            print("PasswordComplexity is already set to 1.")
-            return False
-        else:
-            updated_data = re.sub(pattern, 'PasswordComplexity = 1', data)
-            with open("C:\\winsec.inf", 'w', encoding="utf-16") as file:
-                file.write(updated_data)
-            subprocess.run('secedit /configure /db C:\\test.sdb /cfg C:\\winsec.inf /areas SECURITYPOLICY', shell=True)
-            return True
-    except IOError as e:
-        print(f"File error: {e}")
+    # 이미 PasswordComplexity = 1 이면 문구 출력 후 리턴
+    if match and match.group(1) == '1':
+        print("PasswordComplexity is already set to 1.")
         return False
+    # PasswordComplexity = 0 을 1로 변경후 보안 설정 저장
+    else:
+        updated_data = re.sub(pattern, 'PasswordComplexity = 1', data)
+        with open("C:\\winsec.inf", 'w', encoding="utf-16") as file:
+            file.write(updated_data)
+        subprocess.run('secedit /configure /db C:\\test.sdb /cfg C:\\winsec.inf /areas SECURITYPOLICY', shell=True)
+        return True
 
 def check_pwcomplexity():
     # 보안 설정을 임시 파일로 내보내기
     subprocess.run("secedit /export /cfg C:\\tempsec.inf", shell=True)
 
     # 파일에서 PasswordComplexity 값 찾기
-    try:
-        with open("C:\\tempsec.inf", "r", encoding="utf-16") as file:
-            contents = file.read()
-            match = re.search(r"PasswordComplexity\s*=\s*(\d+)", contents)
-            if int(match.group(1)) == 1:
-                return True
-            else:
-                return False
-    except FileNotFoundError:
-        print("The security settings file could not be found.")
-        return False
+    with open("C:\\tempsec.inf", "r", encoding="utf-16") as file:
+        contents = file.read()
+        match = re.search(r"PasswordComplexity\s*=\s*(\d+)", contents)
+        if int(match.group(1)) == 1:
+            return True
+        else:
+            return False
 
 # 결과 확인
 if change_pwcomplexity():
@@ -52,3 +45,7 @@ if check_pwcomplexity():
     print("Password complexity is enabled.")
 else:
     print("The password complexity is disabled or cannot be determined.")
+
+# 테스트를 위한 무한루프 실행
+while True:
+    print("loop for test")
