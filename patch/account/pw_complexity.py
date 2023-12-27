059ffa6fb3e5f4dd46c6ca84c602b045b6ac47d4
EUN <617julie@korea.ac.kr>
2023-12-27 14:18:19 +0900

    pw_complexity함수 업데이트

--- patch/account/pw_complexity.py
+++ patch/account/pw_complexity.py
@@ -3,6 +3,27 @@
 import subprocess
 import re
 
+def change_pwcomplexity():
+    subprocess.run('secedit /export /cfg C:\\winsec.inf', check=True, shell=True)
+    try:
+        with open("C:\\winsec.inf", 'r', encoding="utf-16") as file:
+            data = file.read()
+        pattern = r'PasswordComplexity\s*=\s*(\d)'
+        match = re.search(pattern, data)
+
+        if match and match.group(1) == '1':
+            print("PasswordComplexity is already set to 1.")
+            return False
+        else:
+            updated_data = re.sub(pattern, 'PasswordComplexity = 1', data)
+            with open("C:\\winsec.inf", 'w', encoding="utf-16") as file:
+                file.write(updated_data)
+            subprocess.run('secedit /configure /db C:\\test.sdb /cfg C:\\winsec.inf /areas SECURITYPOLICY', shell=True)
+            return True
+    except IOError as e:
+        print(f"File error: {e}")
+        return False
+
 def check_pwcomplexity():
     # 보안 설정을 임시 파일로 내보내기
     subprocess.run("secedit /export /cfg C:\\tempsec.inf", shell=True)
@@ -21,6 +42,9 @@ def check_pwcomplexity():
         return False
 
 # 결과 확인
+if(change_pwcomplexity()):
+    print("PasswordComplexity changed to 1")
+
 if check_pwcomplexity():
     print("Password complexity is enabled.")
 else:
