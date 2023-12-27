11430e0efc0a3a195a6308e1ab37ca09a89fe33d
EUN <617julie@korea.ac.kr>
2023-12-27 16:23:56 +0900

    force_exit.py update

--- patch/security/force_exit.py
+++ patch/security/force_exit.py
@@ -8,18 +8,19 @@ def remote_shutdown():
     subprocess.run("Secedit /Export /Areas User_Rights /cfg c:\\privilege.inf", shell=True)
 
     # 파일 수정
-    with open("privilege.inf", "r", encoding="utf-16") as file:
-        lines = file.readlines()
+    with open("c:\\privilege.inf", "r", encoding="utf-16") as file:
+        data = file.read()
+    pattern = r'(SeRemoteShutdownPrivilege\s*=\s*).*'
+    match = re.search(pattern, data)
 
-    with open("privilege.inf", "w", encoding="utf-16") as file:
-        for line in lines:
-            if "SeRemoteShutdownPrivilege" in line:
-                # 관리자 그룹(S-1-5-32-544)을 제외한 모든 SID 제거
-                line = re.sub(r"(SeRemoteShutdownPrivilege\s*=\s*).*", r"\1*S-1-5-32-544,", line)
-            file.write(line)
+    with open("c:\\privilege.inf", "w", encoding="utf-16") as file:
+        if match:
+            # 관리자 그룹(S-1-5-32-544)을 제외한 모든 SID 제거
+            update_data = re.sub(pattern, "SeRemoteShutdownPrivilege = *S-1-5-32-544", data)
+            file.write(update_data)
 
     # 수정된 설정 적용
-    subprocess.run("secedit /configure /db C:\\Windows\\security\\local.sdb /cfg c:\\path\\filename.inf /areas USER_RIGHTS", shell=True)
+    subprocess.run("secedit /configure /db C:\\Windows\\security\\local.sdb /cfg c:\\privilege.inf /areas USER_RIGHTS", shell=True)
     return True
 
 def check_Privilege():
@@ -27,19 +28,15 @@ def check_Privilege():
     subprocess.run("secedit /export /cfg C:\\tempsec.inf", shell=True)
 
     # 파일에서 SeRemoteShutdownPrivilege 값 찾기
-    try:
-        with open("C:\\tempsec.inf", "r") as file:
-            contents = file.read()
-            match = re.search(r"SeRemoteShutdownPrivilege\s*=\s*(.*)", contents)
-            if match:
-                # 관리자 그룹만 있는지 확인
-                privileges = match.group(1).split(',')
-                return privileges == ['*S-1-5-32-544']
-            else:
-                return False
-    except FileNotFoundError:
-        print("The security settings file could not be found.")
-        return False
+    with open("C:\\tempsec.inf", "r", encoding="utf-16") as file:
+        contents = file.read()
+        match = re.search(r"SeRemoteShutdownPrivilege\s*=\s*(.*)", contents)
+        if match:
+            # 관리자 그룹만 있는지 확인
+            privileges = match.group(1).split(',')
+            return privileges == ['*S-1-5-32-544']
+        else:
+            return False
 
 # 결과 확인
 if remote_shutdown():
@@ -49,3 +46,7 @@ if check_Privilege():
     print("Now, Only the Administrator group has remote shutdown privilege.")
 else:
     print("The remote shutdown is not set correctly or cannot be determined.")
+
+# 테스트를 위한 무한루프 실행
+while True:
+    print("loop for test")
