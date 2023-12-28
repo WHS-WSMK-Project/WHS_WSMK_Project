b4c0f2498159c4b53f743cc1757a03d7f614afaf
yejiiiin <109707358+yejiiiin@users.noreply.github.com>
2023-12-28 15:58:02 +0900

    Update remove_mapping.py

--- patch/service/remove_mapping.py
+++ patch/service/remove_mapping.py
@@ -1,29 +1,18 @@
-import xml.etree.ElementTree as ET
+import re
 
-def remove_handler(xml_path, handler_name):
-    # XML 파일 불러오기
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
+def remove_script_mappings(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
 
-    # 삭제할 핸들러 경로
-    handler_path = f'.//handlers/add[@name="{handler_name}"]'
+    pattern = re.compile(r'<handlers accessPolicy="Read, Script">.*?</handlers>', re.DOTALL)
 
-    # 핸들러 노드 찾기 및 삭제
-    handler_nodes = root.findall(handler_path)
-    if handler_nodes:
-        for node in handler_nodes:
-            parent_node = node.getparent()
-            parent_node.remove(node)
+    content = re.sub(pattern, '', content)
 
-        # 변경된 내용 저장
-        tree.write(xml_path)
-        print(f"Handler '{handler_name}' removed successfully")
-    else:
-        print(f"Cannot find handler '{handler_name}' to remove")
+    with open(file_path, 'w') as file:
+        file.write(content)
 
-# 파일 경로와 제거할 핸들러 이름 설정
-xml_file_path = 'C:\\Windows\\System32\\inetsrv\\config\\applicationHost.config'
-handler_to_remove = 'ASPClassic'
+config_file_path = r'C:\Windows\System32\inetsrv\config\applicationHost.config'
 
-# 핸들러 제거 함수 호출
-remove_handler(xml_file_path, handler_to_remove)
+remove_script_mappings(config_file_path)
+
+print("Script mappings removed successfully.")
