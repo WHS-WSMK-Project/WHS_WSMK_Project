71dd6f57713eb0d9f971afc8e3042aeb5f6d1eca
EUN <617julie@korea.ac.kr>
2023-12-28 18:05:34 +0900

    edit for exe

--- patch/main.py
+++ patch/main.py
@@ -65,8 +65,8 @@ def main():
     disable_link.disable_directory_browsing()
 
     # W-17 : 2. 서비스관리 > 2.11 IIS 파일 업로드 및 다운로드 제한
-    limit_bytes = 1048576 # 업로드 및 다운로드 크기 제한을 1MB로 설정
-    print(f"Current upload and download limit: {current_limit} bytes")
+    limit_bytes = 1048576
+    print(f"Current upload and download limit: {limit_bytes} bytes")
     limit_upload_download.set_limit(limit_bytes)
 
     # W-21 : 2. 서비스관리 > 2.15 IIS 미사용 스크립트 매핑 제거
